e80ad2c8cf558dbc351956b2c837eb59c78c1c5e
Tan <thaiminhnhattan2808@gmail.com>
2023-03-22 12:04:27 +0700

    Make Player
    Đã có chức năng di chuyển,  lướt, tấn công
    Đã có animation di chuyển, lướt, die.
    Còn thiếu hình ảnh của vũ khí tấn công và đạn

---
+++ Assets/Scripts/PlayerFindEnemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFindEnemy : MonoBehaviour
+{
+	public Transform direction_attack;
+
+	void Update()
+	{
+		FindClosestEnemy();
+
+	}
+
+	void FindClosestEnemy()
+	{
+		float distanceToClosestEnemy = Mathf.Infinity;
+		Enemy closestEnemy = null;
+		Enemy[] allEnemies = GameObject.FindObjectsOfType<Enemy>();
+
+		foreach (Enemy currentEnemy in allEnemies)
+		{
+			float distanceToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+			if (distanceToEnemy < distanceToClosestEnemy)
+			{
+				distanceToClosestEnemy = distanceToEnemy;
+				closestEnemy = currentEnemy;
+			}
+		}
+		if (closestEnemy == null)
+		{
+			// nếu không có enemy nào còn lại, không làm gì cả
+			return;
+
+		}
+		Debug.DrawLine(this.transform.position, closestEnemy.transform.position);
+		direction_attack = closestEnemy.transform;
+
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public Vector2 direction;
+    [SerializeField] private float speed = 3;
+
+    Animator ani;
+
+    //dash
+    private TrailRenderer tr;
+    private bool canDash = true;
+    private bool isDashing;
+    private float DashingPower = 20f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+
+    //Check player isLive
+    PlayerHealth pl_h;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        tr = GetComponent<TrailRenderer>();
+        pl_h = GetComponent<PlayerHealth>();
+    }
+
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+
+
+        ani.SetFloat("Horizontal", direction.x);
+        ani.SetFloat("Vertical", direction.y);
+        ani.SetFloat("Speed", direction.sqrMagnitude);
+
+        if (Input.GetKey(KeyCode.LeftShift) && canDash && !pl_h.isDie)
+        {
+            StartCoroutine(Dash());
+        }
+
+        if (pl_h.isDie)
+            rb.velocity = Vector2.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDashing)
+        {
+            return;
+        }
+        if(!pl_h.isDie)
+            rb.MovePosition(rb.position + direction * speed * Time.deltaTime);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(direction.x * DashingPower, direction.y * DashingPower);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
+
+}
