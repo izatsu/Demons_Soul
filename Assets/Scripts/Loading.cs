8ccdf525b0efbf0a4ea1b5f810ddd21092517ca3
Tan <thaiminhnhattan2808@gmail.com>
2023-03-24 11:36:32 +0700

    Make UI MainMenu
    Làm nút Play và chức năng loading

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    public GameObject LoaderUI;
+    public Slider progressSlider;
+
+    public void LoadScene(int index)
+    {
+        StartCoroutine(LoadScene_Coroutine(index));
+    }
+
+    public IEnumerator LoadScene_Coroutine(int index)
+    {
+        progressSlider.value = 0;
+        LoaderUI.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(1);
+        asyncOperation.allowSceneActivation = false;
+        float progress = 0;
+
+        while (!asyncOperation.isDone)
+        {
+            progress = Mathf.MoveTowards(progress, asyncOperation.progress, Time.deltaTime);
+            progressSlider.value = progress;
+            if (progress >= 0.9f)
+            {
+                progressSlider.value = 1;
+                asyncOperation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
